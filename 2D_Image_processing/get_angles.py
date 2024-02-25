47e0d8309618ae821fe9229c2eeeeef40b7a9b23
anishdabhane <anishdabhane@gmail.com>
2024-02-25 08:59:28 +0530

    script improved

--- 2D_Image_processing/get_angles.py
+++ 2D_Image_processing/get_angles.py
@@ -6,9 +6,27 @@ from pprint import pprint
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
-image_path = "/home/anishdabhane/Projects/2nd_year/Troops/src/Mediapipe/images/img1.jpeg"
+image_path = "/home/anishdabhane/Projects/2nd_year/Troops/src/Mediapipe/images/img2.jpeg"
 img = cv2.imread(image_path)
 
+def calculate_angle(joint):
+    global all_nodes
+    #print(joint)
+    a = all_nodes[joint[0]]
+    b = all_nodes[joint[1]]
+    c = all_nodes[joint[2]]
+
+    start = np.array(a) # First
+    middle = np.array(b) # Mid
+    end = np.array(c) # End
+
+    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+    angle = np.abs(radians*180.0/np.pi)
+    
+    if angle >180.0:
+        angle = 360-angle
+        
+    return angle 
 
 if img is None:
     print("Could not open or find the image.")
@@ -38,61 +56,26 @@ else:
             z = landmark.z
             all_nodes.append((x, y, z)) 
 
+        # Joints
+        joints = {
+            "left_elbow": [15, 13, 11],
+            "right_elbow": [16, 14, 12],
+            "left_shoulder": [13, 11, 23],
+            "right_shoulder": [14, 12, 24],
+            "left_hip": [25, 23, 24],
+            "right_hip": [26, 24, 23],
+            "left_knee": [23, 25, 27],
+            "right_knee": [24, 26, 28],
+        }
+
+        for key,value in joints.items():
+            print(f"{key} : {calculate_angle(value)}")
+
         cv2.imshow('Mediapipe Feed', img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-def calculate_angle(joint):
-    global all_nodes
-    #print(joint)
-    a = all_nodes[joint[0]]
-    b = all_nodes[joint[1]]
-    c = all_nodes[joint[2]]
-
-    start = np.array(a) # First
-    middle = np.array(b) # Mid
-    end = np.array(c) # End
-
-    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
-    angle = np.abs(radians*180.0/np.pi)
-    
-    if angle >180.0:
-        angle = 360-angle
-        
-    return angle 
-    """
-    SM = middle - start
-    ME = end - middle
-
-    # Calculate the dot product
-    dot_product = np.dot(SM, ME)
-
-    # Calculate the magnitudes of vectors
-    magnitude_SM = np.linalg.norm(SM)
-    magnitude_ME = np.linalg.norm(ME)
-
-    # Calculate the angle in radians
-    theta_radians = np.arccos(dot_product / (magnitude_SM * magnitude_ME))
-
-    # Convert the angle to degrees
-    angle = np.degrees(theta_radians)
-
-    return angle
-    """
 
 
 
-# Joints
-joints = {
-    "elbow_left": [15, 13, 11],
-    "elbow_right": [16, 14, 12],
-    "shoulder_left": [13, 11, 23],
-    "shoulder_right": [14, 12, 24],
-    "hip_left": [25, 23, 24],
-    "hip_right": [26, 24, 23],
-    "knee_left": [23, 25, 27],
-    "knee_right": [24, 26, 28],
-}
 
-for key,value in joints.items():
-    print(f"{key} : {calculate_angle(value)}")
